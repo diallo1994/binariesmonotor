b47024fbbb508f656a10a707862d0c46eace0160
diallo1994 <amadouamy1994@gmail.com>
2021-02-10 11:43:07 +0400

    /usr/local/bin/config-promtail.yml

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@ sudo useradd --system promtail
 sudo service promtail start
 sudo service promtail status
 
-/usr/local/bin/promtail
+usermod -a -G systemd-journal promtail
 
 mv promtail.service /etc/systemd/system
