e66321647289c40dbaca5fe2c9c2a91e3f703d77
diallo1994 <amadouamy1994@gmail.com>
2021-02-18 10:53:21 +0400

    add more labels

--- script.sh
+++ script.sh
@@ -6,3 +6,13 @@ sudo service promtail status
 usermod -a -G systemd-journal promtail
 
 mv promtail.service /etc/systemd/system
+
+sudo systemctl daemon-reload
+sudo systemctl start promtail
+sudo journalctl -o verbose __PID=
+sudo journal
+sudo journalctl -u promtail.service
+
+sudo journalctl -u promtail.service --since "2021-2-13 18:45:00"
+
+sudo chown promtail:promtail path
